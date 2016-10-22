d1bed108332b49fa0645986395d1e35cc489e939
Sutirtha Marjit <sutirtha.social.marjit@gmail.com>
2016-10-22 23:22:45 +0530

    Express update

---
+++ engine.js
@@ -0,0 +1,20 @@
+
+/* ALL IMPORT JOBS : Start*/
+var Chalk = require("chalk");
+var Express = require("express");
+var BodyParser = require("body-parser");
+/* ALL IMPORT JOBS : End*/
+
+console.log(Chalk.green("Application started at "+new Date()));
+console.log('All import jobs done without any error');
+
+var app = Express();
+app.use(Express.static('webapp'));
+
+
+
+app.listen(3000,function(){
+    console.log('working....');
+});
+console.log(process.env)
+
