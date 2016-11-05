6444dce73114c613d0de39179888aba79fbfb6db
Sutirtha Marjit <sutirtha.social.marjit@gmail.com>
2016-11-05 22:46:14 +0530

    Configuration and Search UI update

--- webapp/script/app.config.js
+++ webapp/script/app.config.js
@@ -1,6 +1,10 @@
 empDataMantSystem.config(function($routeProvider) {
 
-    $routeProvider.when('/', { templateUrl: 'templates/emplistview.tpl.html', controller: null });
-    $routeProvider.when('/add', { templateUrl: 'templates/registration.tpl.html', controller: RegistrationController });
+    $routeProvider.when('/', { templateUrl: 'templates/emplistviewupdate.tpl.html', controller: null });
+    $routeProvider.when('/add', { templateUrl: 'templates/registration.tpl.html', controller: null });
+    $routeProvider.when('/update', { templateUrl: 'templates/emplistviewupdate.tpl.html', controller: null });
+    $routeProvider.when('/view', { templateUrl: 'templates/emplistview.tpl.html', controller: null });
+    $routeProvider.when('/search', { templateUrl: 'templates/searchresult.tpl.html', controller: null });
+    $routeProvider.when('/configuration', { templateUrl: 'templates/configuration.tpl.html', controller: null });
 
 });
--- webapp/script/app.domcontrol.js
+++ webapp/script/app.domcontrol.js
@@ -1,13 +1,23 @@
 var DOM_CONTROL = {
+
+    LinkHighlight: function() {
+        var anchors = document.querySelectorAll('a');
+        for (var i = 0; i < anchors.length; i++) {
+            if(anchors[i].href === window.location.href){
+                anchors[i].setAttribute('class','active');
+            }
+        }
+        // window.location.hash
+    },
     NavSelection: function(selection) {
         var links = document.querySelectorAll(selection);
         if (links.length > 0) {
             for (var i = 0; i < links.length; i++) {
                 links[i].addEventListener('click', function(e) {
                     var active = document.querySelector('a.active');
-                    if(active!==null){
+                    if (active !== null) {
                         active.setAttribute('class', '');
-                    }                    
+                    }
                     this.setAttribute('class', 'active');
                 });
             }
@@ -16,6 +26,7 @@ var DOM_CONTROL = {
 };
 
 window.onload = function() {
+    DOM_CONTROL.LinkHighlight();
     DOM_CONTROL.NavSelection('.nav-pills li a');
     DOM_CONTROL.NavSelection('div.console-panel ul li a');
 };
